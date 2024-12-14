b336e790510ecbe7ce17b8036557f7490da319e6
sato-susumu <75652942+sato-susumu@users.noreply.github.com>
2024-12-14 22:01:58 +0900

    Support playback speed

--- README.md
+++ README.md
@@ -2,8 +2,29 @@
 ## 機能
 gTTSを利用して、テキストから音声を生成するノードです。
 
+## ノード起動
+再生速度1.5倍、日本語で起動
+```
+ros2 run susumu_gtts susumu_gtts
+```
+
+再生速度1.5倍、英語で起動
+```
+ros2 run susumu_gtts susumu_gtts --ros-args -p lang:=en -p playback_speed:=1.0
+```
+
+## パラメータ変更
+言語を英語に変更
+```
+ros2 param set /susumu_gtts lang en
+```
+
+再生速度を1.0倍に変更(デフォルトは1.5倍)
+```
+ros2 param set /susumu_gtts playback_speed 1.0
+```
 
 ## 音声再生テスト
 ```
-ros2 topic pub -1 /tts std_msgs/String "data: '次の音声を再生します。'"
+ros2 topic pub -1 /tts std_msgs/String "data: 'ピカチュー、1000万ボルト！'"
 ```
--- susumu_gtts/susumu_gtts.py
+++ susumu_gtts/susumu_gtts.py
@@ -1,20 +1,21 @@
+import pydub
 import rclpy
 from rclpy.node import Node
+from rcl_interfaces.msg import SetParametersResult
 from std_msgs.msg import String
 from gtts import gTTS
 import simpleaudio as sa
 import threading
 import io
-import pydub
 
 
 class SusumuGTTS(Node):
     def __init__(self):
         super().__init__('susumu_gtts')
-
-        # 言語設定用のパラメータを宣言（デフォルトは日本語 'ja'）
         self.declare_parameter('lang', 'ja')
-        self.language = self.get_parameter('lang').get_parameter_value().string_value
+        self.declare_parameter('playback_speed', 1.5)
+        self.language = self.get_parameter('lang').value
+        self.playback_speed = self.get_parameter('playback_speed').value
 
         self.subscription = self.create_subscription(
             String,
@@ -22,29 +23,28 @@ class SusumuGTTS(Node):
             self.speech_callback,
             10)
 
+        self.add_on_set_parameters_callback(self.dynamic_parameter_callback)
+
         self.playback_thread = None
         self.stop_playback = False
         self.lock = threading.Lock()
-        self.get_logger().info(f"Susumu gTTS Node started. Subscribed to /tts topic. Language: {self.language}")
+        self.get_logger().info(f"Susumu gTTS Node started. Subscribed to /tts topic.")
+        self.get_logger().info(f"Language: {self.language}, Playback Speed: {self.playback_speed}x")
 
     def speech_callback(self, msg):
         text = msg.data
         self.get_logger().info(f"Received text: '{text}'")
 
-        # 再生中の音声を停止
         with self.lock:
             if self.playback_thread and self.playback_thread.is_alive():
                 self.get_logger().info("Stopping current playback.")
                 self.stop_playback = True
-                self.playback_thread.join()  # スレッドの終了を待つ
+                self.playback_thread.join()
                 self.stop_playback = False
-
-        # 新しい音声を再生
         self.playback_thread = threading.Thread(target=self.play_audio, args=(text,))
         self.playback_thread.start()
 
     def play_audio(self, text):
-        """ gTTSを使用してメモリ上で音声を生成し再生 """
         try:
             self.get_logger().info(f"Generating audio with language '{self.language}'...")
             tts = gTTS(text=text, lang=self.language)
@@ -52,14 +52,13 @@ class SusumuGTTS(Node):
             tts.write_to_fp(audio_buffer)
             audio_buffer.seek(0)
 
-            # MP3データをWAVに変換して再生
             audio_segment = pydub.AudioSegment.from_file(audio_buffer, format="mp3")
+            if self.playback_speed != 1.0:
+                audio_segment = audio_segment.speedup(playback_speed=self.playback_speed, chunk_size=50, crossfade=0)
             playback_data = audio_segment.raw_data
             playback_obj = sa.play_buffer(playback_data, num_channels=audio_segment.channels,
                                           bytes_per_sample=audio_segment.sample_width,
                                           sample_rate=audio_segment.frame_rate)
-
-            # 再生監視
             while playback_obj.is_playing():
                 if self.stop_playback:
                     playback_obj.stop()
@@ -68,6 +67,16 @@ class SusumuGTTS(Node):
             self.get_logger().info("Playback finished.")
         except Exception as e:
             self.get_logger().error(f"Error during playback: {e}")
+
+    def dynamic_parameter_callback(self, parameters):
+        for param in parameters:
+            if param.name == 'lang' and param.type_ == param.Type.STRING:
+                self.language = param.value
+                self.get_logger().info(f"Language updated to: {self.language}")
+            elif param.name == 'playback_speed' and param.type_ == param.Type.DOUBLE:
+                self.playback_speed = param.value
+                self.get_logger().info(f"Playback speed updated to: {self.playback_speed}")
+        return SetParametersResult(successful=True)
 
 
 def main(args=None):
